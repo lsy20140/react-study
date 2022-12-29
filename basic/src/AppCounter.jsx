7c756d4170c415279e17bf04a39ff6625c77e87c
lsy20140 <lsy20140@naver.com>
2022-12-29 23:30:43 +0900

    Counter 컴포넌트 생성

---
+++ basic/src/AppCounter.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import Counter from './components/Counter'
+import './App.css';
+
+
+export default function AppCounter(props) {
+  const [count, setCount] = useState(0);
+  const handleClick = () => setCount((prev) => prev + 1);
+
+
+  return (
+    <div className='container'>
+      <div className='banner'>
+        Total Count: {count} {count > 10 ? '🔥' : '❄'}
+      </div>
+      <div className='counters'>
+        <Counter total={count} onClick={handleClick}/>
+        <Counter total={count} onClick={handleClick}/>
+      </div>
+
+    </div>
+
+  )
+}
