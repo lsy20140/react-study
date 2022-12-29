a91cbfb3e0be6e5e82a6b0f07a13af56bd46da1c
lsy20140 <lsy20140@naver.com>
2022-12-29 17:05:44 +0900

    기본 세팅

---
+++ basic/src/AppJSX.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function AppJSX() {
+  const name="dd";
+  const list=['우유', '딸기', '바나나'];
+  return (
+    <>
+      <h1>{`Hello, ${name}`}</h1>
+      <ul>
+        {list.map((item) => <li>{item}</li>)}
+      </ul>
+    </>
+
+  );
+}
+
+export default AppJSX;
