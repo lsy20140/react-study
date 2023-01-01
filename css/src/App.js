fc4781205181b4ee86b450bf6d21859f9af4bd4d
lsy20140 <lsy20140@naver.com>
2023-01-02 01:46:01 +0900

    StyledComponent.jsx 생성

--- css/src/App.js
+++ css/src/App.js
@@ -1,13 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 import Button1 from './components/Button1';
 import Button2 from './components/Button2';
+import StyledComponent from './components/StyledComponent';
 
 function App() {
   return (
     <>
       <Button1/>
       <Button2/>
+      <StyledComponent/>
+
     </>
   );
 }
