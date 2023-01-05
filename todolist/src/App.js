32401f1302a931fe52165518c9741ddd4add30a2
lsy20140 <lsy20140@naver.com>
2023-01-05 22:16:21 +0900

    다크모드 구현 완료

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -2,18 +2,18 @@ import './App.css';
 import AppTodolist from './components/AppTodolist/AppTodolist';
 import { useState } from 'react';
 import Header from './components/Header/Header';
+import DarkModeProvider from './context/DarkModeContext';
 
 const filters = ['all', 'active', 'completed'];
 
 
 function App() {
   const [filter, setFilter] = useState(filters[0]); 
-
   return (
-    <>
+    <DarkModeProvider>
       <Header filters={filters} filter={filter} onFilterChange={setFilter}></Header>
       <AppTodolist filter={filter}/>
-    </>
+    </DarkModeProvider>
   );
 }
 
--- todolist/src/components/Header/Header.jsx
+++ todolist/src/components/Header/Header.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { useDarkMode } from '../../context/DarkModeContext'
 import styles from './Header.module.css'
+import { MdLightMode, MdDarkMode } from 'react-icons/md';
 
 export default function Header({filters, filter, onFilterChange}) {
+  const {darkMode, toggleDarkMode} = useDarkMode();
 
   return (
     <header className={styles.header}>
+      <button onClick={toggleDarkMode} className={styles.toggle}>
+        {!darkMode && <MdDarkMode/>}
+        {darkMode && <MdLightMode/>}
+      </button>
       <ul className={styles.filters}>
         {filters.map((value, index) => (
           <li key={index}>
