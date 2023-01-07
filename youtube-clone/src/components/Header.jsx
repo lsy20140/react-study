6eb1e60d393b4c98f80c348aab508945e00eab5c
lsy20140 <lsy20140@naver.com>
2023-01-08 01:27:20 +0900

    Searchbox css 수정

--- youtube-clone/src/components/Header.jsx
+++ youtube-clone/src/components/Header.jsx
@@ -3,8 +3,8 @@ import Searchbox from './Searchbox'
 
 export default function Header() {
   return (
-    <header className='w-3/4 flex items-center justify-between'>
-      <img className='w-36 h-10 mr-24' src='YouTube-White-Logo.png'/>
+    <header className='w-4/5 flex items-center justify-between'>
+      <img className='w-36 h-10' src='YouTube-White-Logo.png'/>
       <Searchbox/>
     </header>
   )
--- youtube-clone/src/components/Searchbox.jsx
+++ youtube-clone/src/components/Searchbox.jsx
@@ -4,9 +4,9 @@ import { BiSearch } from 'react-icons/bi';
 
 export default function Searchbox() {
   return (
-    <form className='w-4/6 h-10 flex items-center justify-center'>
-      <input className='w-full h-full  bg-black pl-1.5' type='text' placeholder='Search...'></input>
-      <button className='w-max h-full px-3 overflow-hidden bg-zinc-700'><BiSearch/></button>
+    <form className='w-3/4 flex px-4 py-6'>
+      <input className='w-full flex-initial bg-black text-base px-3 py-2 border-none outline-none' type='text' placeholder='Search...'></input>
+      <button className='text-2xl cursor-pointer py-0 px-3 text-white bg-zinc-700'><BiSearch/></button>
     </form>
   )
 }
