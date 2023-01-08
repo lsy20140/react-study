b3c550b710d43a1535ea4c370abf1b81d44d9904
lsy20140 <lsy20140@naver.com>
2023-01-08 17:25:07 +0900

    initial commit

--- youtube-clone/src/components/Header.jsx
+++ youtube-clone/src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Searchbox from './Searchbox'
+import logo from '../assets/images/YouTube-White-Logo.png';
 
 export default function Header() {
   return (
     <header className='w-4/5 flex items-center justify-between'>
-      <img className='w-36 h-10' src='YouTube-White-Logo.png'/>
+      <img className='w-36 h-10' src={logo} alt='logo!!!'/>
       <Searchbox/>
     </header>
   )
--- youtube-clone/src/components/Searchbox.jsx
+++ youtube-clone/src/components/Searchbox.jsx
@@ -5,8 +5,8 @@ import { BiSearch } from 'react-icons/bi';
 export default function Searchbox() {
   return (
     <form className='w-3/4 flex px-4 py-6'>
-      <input className='w-full flex-initial bg-black text-base px-3 py-2 border-none outline-none' type='text' placeholder='Search...'></input>
-      <button className='text-2xl cursor-pointer py-0 px-3 text-white bg-zinc-700'><BiSearch/></button>
+      <input className='w-full bg-black text-base px-3 border-none outline-none' type='text' placeholder='Search...'></input>
+      <button className='text-2xl cursor-pointer py-2 px-2 text-white bg-zinc-700'><BiSearch/></button>
     </form>
   )
 }
