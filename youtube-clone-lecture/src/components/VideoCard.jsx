7080469f7105e70853b8880c93630281f19fc36c
lsy20140 <lsy20140@naver.com>
2023-01-12 06:31:28 +0900

    detail 페이지로 navigate+ 각 detail 페이지-영상, 제목, 설명 출력

--- youtube-clone-lecture/src/components/VideoCard.jsx
+++ youtube-clone-lecture/src/components/VideoCard.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import '../index.css'
 import { formatAgo } from '../util/date';
+import { useNavigate } from 'react-router-dom'
 
 export default function VideoCard({video}) {
   const {title, thumbnails, channelTitle, publishedAt} = video.snippet;
+  const navigate = useNavigate();
 
   return (
-  <li>
-    <img className='w-full' src={thumbnails.medium.url} alt={title}/>
+  <li onClick={() => {navigate(`videos/watch/${video.id}`, {state: {video}})} }>
+    <img className='cursor-pointer w-full' src={thumbnails.medium.url} alt={title}/>
     <div>
-      <p className='my-2 font-semibold line-clamp-2' >{title}</p>
+      <p className='cursor-pointer my-2 font-semibold line-clamp-2' >{title}</p>
       <p className='text-sm  opacity-80'>{channelTitle}</p>
       <p className='text-sm  opacity-80'>{formatAgo(publishedAt, 'ko')}</p>
     </div>
